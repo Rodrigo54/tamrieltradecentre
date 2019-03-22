beff2396c9b848bd1952bbb7bb348a31dbb4d58d
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2019-03-21 22:44:33 -0300

    update search box

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged, startWith, switchMap, tap, filter } from 'rxjs/operators';
 
@@ -14,32 +14,42 @@ import { Router, NavigationExtras } from '@angular/router';
 export class SearchBoxComponent implements OnInit {
   stateCtrl = new FormControl('', { updateOn: 'change' });
   filteredStates: Observable<any[]>;
+  form: FormGroup;
   constructor(
     private service: DatabaseService,
-    private router: Router
-  ) {
+    private router: Router,
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      stateCtrl: this.stateCtrl
+    });
     this.filteredStates = this.stateCtrl.valueChanges.pipe(
       startWith(''),
       filter(i => i  !== ''),
-      filter((i: string) => i.length > 3),
+      filter((i: string) => i.length > 2),
       debounceTime(500),
       distinctUntilChanged(),
       switchMap(state => this.service.autoComplete(state))
     );
   }
 
-  ngOnInit() {}
-  onSubmit(item) {
-    const { value } = item.option;
+  onSubmit(item?: any) {
     const navigationExtras: NavigationExtras = {
       queryParams: {
-        ItemID: value.ItemID,
         SortBy: 'LastSeen',
         Order: 'desc',
         lang: 'en-US',
         page: '1'
       },
     };
+    if (item) {
+      const { value } = item.option;
+      navigationExtras.queryParams.ItemID = value.ItemID;
+    } else {
+      navigationExtras.queryParams.ItemNamePattern = this.stateCtrl.value;
+    }
     this.router.navigate(['/SearchResult'], navigationExtras);
   }
 
